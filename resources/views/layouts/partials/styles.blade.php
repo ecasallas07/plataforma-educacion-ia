a1ceed49ccc6e10f83d586d15381fa07a01d0008
Esteban David Hernández Casallas <esteban.hernandez@parservicios.com>
2024-09-09 22:58:51 -0500

    Implementation temaplete, adn progress migrations

---
+++ resources/views/layouts/partials/styles.blade.php
@@ -0,0 +1,28 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="http://localhost:1313//app.css">
+<link rel="apple-touch-icon" sizes="180x180" href="http://localhost:1313/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="http://localhost:1313/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="http://localhost:1313/favicon-16x16.png">
+<link rel="icon" type="image/png" href="http://localhost:1313/favicon.ico">
+<link rel="manifest" href="http://localhost:1313/site.webmanifest">
+<link rel="mask-icon" href="http://localhost:1313/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+
+{{--    <!-- Twitter -->--}}
+{{--    <meta name="twitter:card" content="summary_large_image">--}}
+{{--    <meta name="twitter:site" content="@">--}}
+{{--    <meta name="twitter:creator" content="@">--}}
+{{--    <meta name="twitter:title" content="Tailwind CSS Admin Dashboard - Flowbite">--}}
+{{--    <meta name="twitter:description" content="Get started with a free and open-source admin dashboard layout built with Tailwind CSS and Flowbite featuring charts, widgets, CRUD layouts, authentication pages, and more">--}}
+{{--    <meta name="twitter:image" content="http://localhost:1313/">--}}
+
+{{--    <!-- Facebook -->--}}
+{{--    <meta property="og:url" content="http://localhost:1313/">--}}
+{{--    <meta property="og:title" content="Tailwind CSS Admin Dashboard - Flowbite">--}}
+{{--    <meta property="og:description" content="Get started with a free and open-source admin dashboard layout built with Tailwind CSS and Flowbite featuring charts, widgets, CRUD layouts, authentication pages, and more">--}}
+{{--    <meta property="og:type" content="website">--}}
+{{--    <meta property="og:image" content="http://localhost:1313/images/og-image.png">--}}
+{{--    <meta property="og:image:type" content="image/png">--}}
